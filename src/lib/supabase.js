c932fbc46e2e5fafaa191bb45f1c90d11f5c7f41
Lawder093 <zaydak93@outlook.com>
2026-04-23 22:18:22 -0600

    First Commint

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://uoumtesahhiqemqrzyov.supabase.co',
+  'sb_publishable_hX8QBC-_-jVvd8Y2ZpR2gA_tRU5DRnc'
+)
